c92a2a4843d123865737504295dd07a4ec781f4b
Soham Lodh <sohamlodh@Sohams-MacBook-Air.local>
2026-03-26 23:30:03 +0530

    Fix fraud queue registration

--- apps/backend/src/modules/fraud/fraud.module.ts
+++ apps/backend/src/modules/fraud/fraud.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { QUEUE_NAMES } from '../../config/queue.constants';
 import { OfferCompletion } from '../offerwall/entities/offer-completion.entity';
 import { FraudService } from './fraud.service';
 import { FraudProcessor } from './fraud.processor';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OfferCompletion]), UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([OfferCompletion]),
+    UsersModule,
+    BullModule.registerQueue({ name: QUEUE_NAMES.fraud }),
+  ],
   providers: [FraudService, FraudProcessor],
   exports: [FraudService],
 })
