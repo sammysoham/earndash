c2f2efd6a17cd2ee9a53f6629041eba5b2149f18
Soham Lodh <sohamlodh@Sohams-MacBook-Air.local>
2026-03-26 23:54:23 +0530

    Fix withdrawals queue registration

--- apps/backend/src/modules/withdrawals/withdrawals.module.ts
+++ apps/backend/src/modules/withdrawals/withdrawals.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { QUEUE_NAMES } from '../../config/queue.constants';
 import { Withdrawal } from './entities/withdrawal.entity';
 import { WithdrawalsRepository } from './repositories/withdrawals.repository';
 import { WithdrawalsProcessor, WithdrawalsService } from './withdrawals.service';
@@ -9,7 +11,13 @@ import { UsersModule } from '../users/users.module';
 import { AuditModule } from '../audit/audit.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Withdrawal]), WalletModule, UsersModule, AuditModule],
+  imports: [
+    TypeOrmModule.forFeature([Withdrawal]),
+    WalletModule,
+    UsersModule,
+    AuditModule,
+    BullModule.registerQueue({ name: QUEUE_NAMES.withdrawals }),
+  ],
   controllers: [WithdrawalsController],
   providers: [WithdrawalsRepository, WithdrawalsService, WithdrawalsProcessor],
   exports: [WithdrawalsRepository, WithdrawalsService],
