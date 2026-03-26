88c120a9b12067fc4df8a20c93646a2abeb7352a
Soham Lodh <sohamlodh@Sohams-MacBook-Air.local>
2026-03-26 23:47:04 +0530

    Fix rewards queue registration

--- apps/backend/src/modules/rewards/rewards.module.ts
+++ apps/backend/src/modules/rewards/rewards.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
+import { QUEUE_NAMES } from '../../config/queue.constants';
 import { RewardsProcessor, RewardsService } from './rewards.service';
 import { OfferwallModule } from '../offerwall/offerwall.module';
 import { WalletModule } from '../wallet/wallet.module';
@@ -6,7 +8,12 @@ import { AuditModule } from '../audit/audit.module';
 import { RewardsController } from './rewards.controller';
 
 @Module({
-  imports: [OfferwallModule, WalletModule, AuditModule],
+  imports: [
+    OfferwallModule,
+    WalletModule,
+    AuditModule,
+    BullModule.registerQueue({ name: QUEUE_NAMES.rewards }),
+  ],
   controllers: [RewardsController],
   providers: [RewardsService, RewardsProcessor],
   exports: [RewardsService],
